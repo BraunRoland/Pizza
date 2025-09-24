839a1855f30b0e030e003ef8ca7bbbea3688305c
Petrik Diak <diak@petrik.hu>
2025-09-24 09:29:48 +0200

    Add project files.

---
+++ Pizza/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Pizza
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        List<string> teszta = new List<string> { "vékony", "normál", "extra vastag"};
+        List<string> meret = new List<string> { "kicsi", "közepes", "nagy" };
+		public MainWindow()
+        {
+            InitializeComponent();
+            lb_teszta.ItemsSource = teszta;
+            cmb_meret.ItemsSource = meret;
+		}
+    }
+}
